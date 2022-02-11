ddd0fcb1f915cac8dec3f94c62e8a7ca11e96d05
Giulio Starace <giulio.starace@gmail.com>
2022-02-11 17:46:17 +0100

    redirect to profile visibility

--- src/background_script.js
+++ src/background_script.js
@@ -2,15 +2,26 @@
 
 // profile match patterns for LinkedIn
 profileMatchPatterns = ["*://*.linkedin.com/in/*"];
+targetURL = "https://www.linkedin.com/psettings/profile-visibility";
 
 // to check whether the correct URL was intercepted
 function logURL(requestDetails) {
   console.log("Loading: " + requestDetails.url);
 }
 
+function redirectToVis(requestDetails) {
+  console.log("Redirecting to profile visibility page");
+  if (requestDetails.url === targetURL) {
+    // if we were already headed there for some reason
+    console.log("FUCK");
+    return;
+  }
+  return { redirectUrl: targetURL };
+}
+
 // detect if a linkedin profile page is requested and intercept request
 browser.webRequest.onBeforeRequest.addListener(
-  logURL,
+  redirectToVis,
   {
     urls: profileMatchPatterns,
   },
