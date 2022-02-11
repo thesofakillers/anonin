cf4639ff1e9ecb52aa9ae6aeff3d6be0127b6032
Giulio Starace <giulio.starace@gmail.com>
2022-02-11 17:19:53 +0100

    detect linkedin profile requests

--- src/background_script.js
+++ src/background_script.js
@@ -1,1 +1,18 @@
 // Put all the javascript code here, that you want to execute in background.
+
+// profile match patterns for LinkedIn
+profileMatchPatterns = ["*://*.linkedin.com/in/*"];
+
+// to check whether the correct URL was intercepted
+function logURL(requestDetails) {
+  console.log("Loading: " + requestDetails.url);
+}
+
+// detect if a linkedin profile page is requested and intercept request
+browser.webRequest.onBeforeRequest.addListener(
+  logURL,
+  {
+    urls: profileMatchPatterns,
+  },
+  ["blocking"]
+);
