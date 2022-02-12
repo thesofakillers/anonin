454733582851b025a2b2c8857d2478482fb56a7e
Giulio Starace <giulio.starace@gmail.com>
2022-02-12 20:53:35 +0100

    use timeout to make clickwork

--- src/content_script.js
+++ src/content_script.js
@@ -3,10 +3,6 @@ function determineIfChecked(element) {
   return element.hasAttribute("checked");
 }
 
-async function clickCheckbox(element) {
-  await element.click();
-}
-
 // returns which of the elements from a list is checked
 function determineWhichChecked(elements) {
   for (var i = 0; i < elements.length; i++) {
@@ -16,26 +12,31 @@ function determineWhichChecked(elements) {
   }
 }
 
-// parse page
-var fullProfileOpt = document.getElementById("option-full");
-var anonProfileOpt = document.getElementById("option-anonymous");
-var hideProfileOpt = document.getElementById("option-hide");
-var visibilityOptions = [fullProfileOpt, anonProfileOpt, hideProfileOpt];
+// main content script process
+function main() {
+  // parse page
+  var fullProfileOpt = document.getElementById("option-full");
+  var anonProfileOpt = document.getElementById("option-anonymous");
+  var hideProfileOpt = document.getElementById("option-hide");
+  var visibilityOptions = [fullProfileOpt, anonProfileOpt, hideProfileOpt];
 
-// determine current profile visibility
-originalOption = determineWhichChecked(visibilityOptions);
+  // determine current profile visibility
+  originalOption = determineWhichChecked(visibilityOptions);
 
-var visibilitySending = browser.runtime.sendMessage({
-  type: "original-visibility",
-  content: originalOption.id,
-});
+  // browser.runtime.sendMessage({
+  //   type: "original-visibility",
+  //   content: originalOption.id,
+  // });
 
-// click on hide profile option
-if (originalOption.id != hideProfileOpt.id) {
-  clickCheckbox(hideProfileOpt);
+  // click on hide profile option
+  if (originalOption.id != hideProfileOpt.id) {
+    hideProfileOpt.click();
+  }
+
+  // // we are now done
+  // browser.runtime.sendMessage({
+  //   type: "complete-anonymous",
+  // });
 }
 
-// // we are now done
-// var completeSending = browser.runtime.sendMessage({
-//   type: "complete-anonymous",
-// });
+setTimeout(main, 500);
