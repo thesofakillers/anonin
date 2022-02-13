88ef5896ad0e5b107c1c398ba6ded0a94ebe145a
Giulio Starace <giulio.starace@gmail.com>
2022-02-13 18:21:38 +0100

    add try-catch statement

--- src/linkedin_observer.js
+++ src/linkedin_observer.js
@@ -1,16 +1,22 @@
-console.log("observing linkedin");
-
 var profileURLRegex = "^[^:]*:(?://)?(?:[^/]*\\.)?linkedin\\.com/in/.*$";
 var visibilityURL = "https://www.linkedin.com/psettings/profile-visibility";
 
 document.addEventListener("click", (event) => {
-  let closestAnchorLink = event.target.closest("a").href;
-  if (closestAnchorLink.match(profileURLRegex)) {
-    // redirect to google
-    event.preventDefault();
-    browser.runtime.sendMessage({
-      type: "profile-navigation",
-      content: closestAnchorLink,
-    });
+  try {
+    let closestAnchorLink = event.target.closest("a").href;
+    if (closestAnchorLink.match(profileURLRegex)) {
+      // redirect to google
+      event.preventDefault();
+      browser.runtime.sendMessage({
+        type: "profile-navigation",
+        content: closestAnchorLink,
+      });
+    }
+  } catch (e) {
+    if (e instanceof TypeError) {
+      // do nothing
+    } else {
+      throw e;
+    }
   }
 });
